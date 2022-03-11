8027a1912ad23d6628b7a93e7faaf8b162739041
rafael1717y <rafael1717yy@gmail.com>
2022-03-11 09:26:34 -0300

    criação dos forms

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 # Importando a variável app definida dentro pacote app
-from flask import render_template
+from flask import flash, redirect, render_template, url_for
 from app import app
+from app.forms import LoginForm
 
 
 # Decorator para associação do '/' e '/index' com a função
@@ -19,3 +20,16 @@ def index():
         },        
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requestes for user {}, remeber_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
+
+
+
