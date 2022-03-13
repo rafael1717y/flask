4615701d6251f87e02f96f79b5fb7e8e63b8d950
rafael1717y <rafael1717yy@gmail.com>
2022-03-13 17:41:56 -0300

    logs

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,12 @@
-# Importando a variável app definida dentro pacote app
 from datetime import datetime
-from flask import flash, redirect, render_template, request, url_for
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.urls import url_parse
-from flask_login import current_user, login_required, login_user, logout_user, login_required
 from app import app, db
-from app.forms import EditProfileForm, LoginForm, RegistrationForm, EditProfileForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from app.models import User
 
 
-# Decorator para associação do '/' e '/index' com a função
 @app.before_request
 def before_request():
     if current_user.is_authenticated:
@@ -18,17 +16,17 @@ def before_request():
 
 @app.route('/')
 @app.route('/index')
-@login_required   #disponível só para usuários logados
+@login_required
 def index():
     posts = [
         {
-            'author':{'username': 'John'},
-            'body': 'Um belo dia!', 
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
         },
         {
-            'author': {'username':'Sissi'}, 
-            'body': 'Nada aqui!'
-        },        
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
     ]
     return render_template('index.html', title='Home', posts=posts)
 
@@ -51,7 +49,6 @@ def login():
     return render_template('login.html', title='Sign In', form=form)
 
 
-
 @app.route('/logout')
 def logout():
     logout_user()
@@ -68,7 +65,7 @@ def register():
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
-        flash('Congratulations, you are now a register user!')
+        flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
@@ -78,18 +75,16 @@ def register():
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     posts = [
-        {'author':user, 'body': 'Test post #1'},
-        {'author':user, 'body': 'Test post #2'},
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
     ]
     return render_template('user.html', user=user, posts=posts)
-
-
 
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
-    form = EditProfileForm()
+    form = EditProfileForm(current_user.username)
     if form.validate_on_submit():
         current_user.username = form.username.data
         current_user.about_me = form.about_me.data
@@ -100,13 +95,3 @@ def edit_profile():
         form.username.data = current_user.username
         form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', title='Edit Profile', form=form)
-
-
-
-
-
-
-
-
-
-
