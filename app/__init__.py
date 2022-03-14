c0f6a6dc2b09cdcc376c2a000677aabebf1bb84d
rafael1717y <rafael1717yy@gmail.com>
2022-03-14 10:13:30 -0300

    pagination

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,10 @@ import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
 from flask import Flask
-from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-
+from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -14,7 +13,6 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
-
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
@@ -44,5 +42,4 @@ if not app.debug:
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
 
-# esse import deve estar aqui (circular dependence)
 from app import routes, models, errors
--- app/models.py
+++ app/models.py
@@ -1,9 +1,9 @@
-from email.policy import default
-from hashlib import md5
-from flask_login import UserMixin
 from datetime import datetime
-from werkzeug.security import generate_password_hash, check_password_hash
+from hashlib import md5
 from app import db, login
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 followers = db.Table(
     'followers',
@@ -24,52 +24,45 @@ class User(UserMixin, db.Model):
         'User', secondary=followers,
         primaryjoin=(followers.c.follower_id == id),
         secondaryjoin=(followers.c.followed_id == id),
-        backref = db.backref('followers', lazy='dynamic'), lazy='dynamic')
-    
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
-
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
-
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
-        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
-        
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
 
     def follow(self, user):
         if not self.is_following(user):
             self.followed.append(user)
 
-
     def unfollow(self, user):
         if self.is_following(user):
             self.followed.remove(user)
-
 
     def is_following(self, user):
         return self.followed.filter(
             followers.c.followed_id == user.id).count() > 0
 
-
     def followed_posts(self):
         followed = Post.query.join(
             followers, (followers.c.followed_id == Post.user_id)).filter(
                 followers.c.follower_id == self.id)
-        return followed.union(self.posts).order_by(Post.timestamp.desc())       
-            
+        own = Post.query.filter_by(user_id=self.id)
+        return followed.union(own).order_by(Post.timestamp.desc())
 
 
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
-
 
 
 class Post(db.Model):
@@ -80,6 +73,3 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
-
-
-
